10cdfa9403803852b4f3162917aced2d40bc0984
xbladev <blademyx@gmail.com>
2025-08-05 21:11:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', 
+    SESSION_ID: process.env.SESSION_ID || 'GENUX-3b1126', 
     PREFIX: process.env.PREFIX || '.',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || [''],
-    MONGODB_URI: process.env.MONGODB_URI || '',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || ['94764497078'],
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://kkasanka9:asanka@sandaru.nuaoou6.mongodb.net/?retryWrites=true&w=majority&appName=sandaru',
 };
+
